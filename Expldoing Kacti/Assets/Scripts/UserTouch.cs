b1790cdf1ee024e4cb3aa4f57e3d7ccb960b16b9
NotNatural21 <89242396+NotNatural21@users.noreply.github.com>
2021-11-01 22:58:05 +1300

    Create UserTouch.cs

---
+++ Expldoing Kacti/Assets/Scripts/UserTouch.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UserTouch : MonoBehaviour
+{ 
+    Collider2D col;
+    bool explode = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        col = GetComponent<Collider2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.touchCount > 0){
+            Touch touch = Input.GetTouch(0);
+            Vector2 touchPosition = Camera.main.ScreenToWorldPoint(touch.position);
+           
+           if (touch.phase == TouchPhase.Began){
+               Collider2D touchedCollider = Physics2D.OverlapPoint(touchPosition);
+               if (col == touchedCollider){
+                   explode = true;
+                   Debug.Log("this was touched");
+               }
+               if (explode == true){
+                   //Destroy(GameObject);
+               }
+            }
+        }
+    }
+    
+}
