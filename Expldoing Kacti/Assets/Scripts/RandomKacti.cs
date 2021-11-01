6c4d99220ce3cb52157f174903a8b4f660362072
NotNatural21 <89242396+NotNatural21@users.noreply.github.com>
2021-11-01 22:58:02 +1300

    Update RandomKacti.cs

--- Expldoing Kacti/Assets/Scripts/RandomKacti.cs
+++ Expldoing Kacti/Assets/Scripts/RandomKacti.cs
@@ -4,20 +4,65 @@ using UnityEngine;
 
 public class RandomKacti : MonoBehaviour
 {
-    //public Sprite sprite;
+    public int[] topY = {60, 70};
+    public int[] bottomY = {-60, -70};
+    public int[] rightX = {120, 130};
+    public int[] leftX = {-120, -130};
 
+    Vector2 spawnPosition;
+
+    // Start is called at the start
     void Start()
-    {
-        var gameObject = new GameObject("Cacti");
-        var spriteRenderer = gameObject.AddComponent<SpriteRenderer>();
-        var sprite = Resources.Load<Sprite>("Kacti/normal_cacti");
-        spriteRenderer.sprite = sprite;
+    {   
+        spawnCacti();
     }
     // Update is called once per frame
     void Update()
     {
         
     }
+
+    Vector2 GetRandomPosition(){
+        float randomX = 125;
+        float randomY = 65;            
+        int randomSide = Random.Range(1, 5);
+        if (randomSide == 1){ // Top of Screen
+            randomX = Random.Range(leftX[1], rightX[1]);
+            randomY = Random.Range(topY[0], topY[1]);
+        }
+        else if(randomSide == 2){ // Left of Screen
+            randomX = Random.Range(rightX[0], rightX[1]);
+            randomY = Random.Range(bottomY[1], topY[1]);
+        }
+        else if(randomSide == 3){ // Bottom of Screen
+            randomX = Random.Range(leftX[1], rightX[1]);
+            randomY = Random.Range(bottomY[0], bottomY[1]);
+        }
+        else if(randomSide == 4){ // Right of Screen
+            randomX = Random.Range(leftX[0], leftX[1]);
+            randomY = Random.Range(bottomY[1], topY[1]);
+        }
+        return new Vector2(randomX, randomY);
+        
+    }
+
+    public void spawnCacti(){
+        spawnPosition = GetRandomPosition();
+        var gameObject = new GameObject("Cacti");
+        gameObject.transform.localScale = new Vector3(14, 14, 1);
+        gameObject.transform.position = new Vector3(spawnPosition[0], spawnPosition[1], 0);
+        gameObject.AddComponent<MoveKacti>();
+        gameObject.AddComponent<UserTouch>();
+        gameObject.AddComponent<CircleCollider2D>();
+        //gameObject.GetComponent<CircleCollider2D>().isTrigger = true;
+
+        var spriteRenderer = gameObject.AddComponent<SpriteRenderer>();
+        var sprite = Resources.Load<Sprite>("Kacti/normal_cacti");
+        spriteRenderer.sprite = sprite;
+
+    }
+
+    
 }
 
 
