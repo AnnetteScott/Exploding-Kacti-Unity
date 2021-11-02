a0ec40e637007a6e5906c6f08603d2b6ea5b6dd4
NotNatural21 <89242396+NotNatural21@users.noreply.github.com>
2021-11-02 13:29:56 +1300

    Update RandomKacti.cs

--- Expldoing Kacti/Assets/Scripts/RandomKacti.cs
+++ Expldoing Kacti/Assets/Scripts/RandomKacti.cs
@@ -4,11 +4,11 @@ using UnityEngine;
 
 public class RandomKacti : MonoBehaviour
 {
-    public int[] topY = {60, 70};
-    public int[] bottomY = {-60, -70};
-    public int[] rightX = {120, 130};
-    public int[] leftX = {-120, -130};
-    public int CactiAllowed = 10;
+    public int[] topY = {6, 11};
+    public int[] bottomY = {-6, -11};
+    public int[] rightX = {11, 16};
+    public int[] leftX = {-11, -16};
+    public int CactiAllowed = 1;
     public int CactiOnScreen = 0;
 
     Vector2 spawnPosition;
@@ -21,32 +21,33 @@ public class RandomKacti : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(CactiOnScreen < CactiAllowed){
-            spawnCacti();
-            CactiOnScreen += 1;
-        }
+
     }
  
     Vector2 GetRandomPosition(){
-        float randomX = 125;
-        float randomY = 65;            
-        int randomSide = Random.Range(1, 5);
-        if (randomSide == 1){ // Top of Screen
-            randomX = Random.Range(leftX[1], rightX[1]);
-            randomY = Random.Range(topY[0], topY[1]);
-        }
-        else if(randomSide == 2){ // Left of Screen
-            randomX = Random.Range(rightX[0], rightX[1]);
-            randomY = Random.Range(bottomY[1], topY[1]);
-        }
-        else if(randomSide == 3){ // Bottom of Screen
-            randomX = Random.Range(leftX[1], rightX[1]);
-            randomY = Random.Range(bottomY[0], bottomY[1]);
-        }
-        else if(randomSide == 4){ // Right of Screen
-            randomX = Random.Range(leftX[0], leftX[1]);
-            randomY = Random.Range(bottomY[1], topY[1]);
-        }
+        float randomX = 0;
+        float randomY = 5;
+        randomX = Random.Range(leftX[1], rightX[1]);
+        //randomY = Random.Range(topY[0], topY[1]);            
+        // int randomSide = Random.Range(1, 5);
+        // if (randomSide == 1){ // Top of Screen
+        //     randomX = Random.Range(leftX[1], rightX[1]);
+        //     randomY = Random.Range(topY[0], topY[1]);
+        // }
+        // else if(randomSide == 2){ // Left of Screen
+        //     randomX = Random.Range(rightX[0], rightX[1]);
+        //     randomY = Random.Range(bottomY[1], topY[1]);
+        // }
+        // else if(randomSide == 3){ // Bottom of Screen
+        //     randomX = Random.Range(leftX[1], rightX[1]);
+        //     randomY = Random.Range(bottomY[0], bottomY[1]);
+        // }
+        // else if(randomSide == 4){ // Right of Screen
+        //     randomX = Random.Range(leftX[0], leftX[1]);
+        //     randomY = Random.Range(bottomY[1], topY[1]);
+        // }
+        Debug.Log(leftX[1]);
+        Debug.Log(rightX[1]);
         return new Vector2(randomX, randomY);
         
     }
@@ -54,7 +55,7 @@ public class RandomKacti : MonoBehaviour
     public void spawnCacti(){
         spawnPosition = GetRandomPosition();
         var gameObject = new GameObject("Cacti");
-        gameObject.transform.localScale = new Vector3(14, 14, 1);
+        gameObject.transform.localScale = new Vector3(1.2F, 1.2F, 1);
         gameObject.transform.position = new Vector3(spawnPosition[0], spawnPosition[1], 0);
         gameObject.AddComponent<MoveKacti>();
         gameObject.AddComponent<UserTouch>();
